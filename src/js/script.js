beb64ea8e6ec2d4223c0ba42a527d9567c48e754
Denys Zhehlov <nordik2121@gmail.com>
2023-02-08 18:40:42 +0200

    modal widnows done

--- src/js/script.js
+++ src/js/script.js
@@ -34,4 +34,20 @@ $(document).ready(function(){
 
     toggleSldie('.catalog-item__link');
     toggleSldie('.catalog-item__back');
+
+    //modal 
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn();
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+
+    $('.button_item').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn();
+        });
+    });
   });
