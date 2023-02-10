3a51c1a48c161777519827d2245ea3978b2edeb4
Denys Zhehlov <nordik2121@gmail.com>
2023-02-10 18:51:31 +0200

    mailer mockup done

--- src/js/script.js
+++ src/js/script.js
@@ -93,8 +93,8 @@ $(document).ready(function(){
             data: $(this).serialize()
         }).done(function() {
             $(this).find('input').val('');
-
-
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
             $('form').trigger('reset');
         });
         return false;
